c9fe4469bfd7c8d140c62b3ca066351234dfb5e6
SenseiMori <mtpchelokot@gmail.com>
2025-07-06 23:31:51 +0500

    init commit

---
+++ WebPicTweak/Controllers/ImagesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PTWeb.Controllers
+{
+    [Route("/api/images")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetImages()
+        {
+            var images = new[]
+            {
+                new { Name = "1" },
+                new { Name = "2" },
+            };
+            return Ok(images);
+        }
+
+    }
+
+
+}
