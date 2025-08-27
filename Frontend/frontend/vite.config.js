b3e306afb9c2bc6bae96466f2b9d306dc86b1943
SenseiMori <mtpchelokot@gmail.com>
2025-08-27 18:50:01 +0500

    edit compose

--- Frontend/frontend/vite.config.js
+++ Frontend/frontend/vite.config.js
@@ -3,16 +3,22 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist', // билд будет в папку dist
+    },
     server: {
+        // Эти настройки нужны только для локальной разработки
         host: true,
-        allowedHosts: ["tweakypro.ru", "localhost"],
         port: 3000,
+        allowedHosts: ["localhost", "tweakypro.ru"],
         proxy: {
             '/api': {
-                target: "http://localhost:5022",
+                target: 'http://localhost:5022', // твой локальный backend
                 changeOrigin: true,
                 secure: false,
             },
         },
+        // Для продакшн HMR отключаем
+        hmr: false,
     },
 });
